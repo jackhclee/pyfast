fc02a97b944549d9b5d77a8176c30874fd17e98e
Jack Lee <jack@uknow.com>
2025-03-30 23:27:08 +0100

    PFAST-0001: Init add

--- src/pyfast/main.py
+++ src/pyfast/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from pyfast.db import repo
 import time
+import logging
+import time
+
+logging.Formatter.converter = time.gmtime
+logging.basicConfig(style="{", format="{asctime}.{msecs:03.0f}Z {levelname} [{processName}] [{threadName}] {name} {message} [{filename}:{lineno}]", datefmt="%Y-%m-%dT%H:%M:%S", level=logging.INFO)
 
 app = FastAPI()
 
 @app.get("/")
 async def m():
-  print(dir(repo))
+  logging.info(f"called def")
   return f"Hello World!!!!!!++ {time.gmtime()} {repo.get_record(99)} {repo.get_all_record()}"
